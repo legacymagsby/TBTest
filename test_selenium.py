c2b54b196e130af7d71a298cf69439c5a285d180
Daphne <legacymagsby@yahoo.com>
2020-10-16 00:13:31 -0500

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
 
 username = os.getenv("BROWSERSTACK_USERNAME")
 access_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
